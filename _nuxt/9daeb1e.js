1c74edd5fa4b00dd1c15f424f6601891672a779d
N1ikls <86553094+N1ikls@users.noreply.github.com>
2022-07-09 18:24:09 +0000

    Deploying to gh-pages from @ N1ikls/test_frontend_developer@c1c5e4359ce88da02c2af9e1ddc596dbe31537c9 🚀

---
+++ _nuxt/9daeb1e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{263:function(n,t,e){"use strict";e.r(t);var l={name:"EditPanel"},c=e(44),component=Object(c.a)(l,(function(){return(0,this._self._c)("div",[this._v("1")])}),[],!1,null,null,null);t.default=component.exports}}]);
